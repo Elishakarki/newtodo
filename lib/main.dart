ef8490ed5865c9efabbeb8c8e679d4267901cce3
Elishakarki <karkielisha222@gmail.com>
2023-03-08 22:04:10 +0545

    todo app with completed and deleted   record

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:addtodo/body.dart';
+import 'package:addtodo/providers/todo_provider.dart';
+import 'package:addtodo/theme.dart';
+
+void main() {
+  runApp(
+    
+    ChangeNotifierProvider(
+      create: (context) => TodoProvider(),
+      builder: (context, child) => const TodoList(),
+    ),
+  );
+}
+
+class TodoList extends StatelessWidget {
+  const TodoList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+      debugShowCheckedModeBanner: false,
+      theme: lightTheme,
+      home: const DefaultTabController(
+        length: 3,
+        child: Body(),
+      )
+    );
+  }
+}
